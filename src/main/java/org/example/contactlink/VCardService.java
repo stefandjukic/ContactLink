800392f6f305db29af42c1aef778436a3aaea540
StefanDjukic4 <stefan.djukic@edu.fit.ba>
2024-11-30 20:36:27 +0100

    initially

--- src/main/java/org/example/contactlink/VCardService.java
+++ src/main/java/org/example/contactlink/VCardService.java
@@ -5,13 +5,14 @@ import org.springframework.stereotype.Service;
 @Service
 public class VCardService {
 
-    public String generateVCard(String name, String phone, String email, String jobTitle, String base64Picture) {
+    public String generateVCard(String name, String phone, String email, String jobTitle,String companyName, String base64Picture) {
         return "BEGIN:VCARD\n" +
                 "VERSION:3.0\n" +
                 "FN:" + name + "\n" +
                 "TEL:" + phone + "\n" +
                 "EMAIL:" + email + "\n" +
                 "TITLE:" + jobTitle + "\n" +
+                "ORG:" + companyName + "\n" +
                 "PHOTO;ENCODING=BASE64;TYPE=JPEG:" + base64Picture + "\n" +
                 "END:VCARD";
 
